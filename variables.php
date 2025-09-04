c31b2144a98061625afea084185d0130e6722e23
Adrian061022 <markusadrian2006@gmail.com>
2025-09-04 11:22:56 +0200

    variables

--- variables.php
+++ variables.php
@@ -5,6 +5,35 @@ $age = 18;
 $city = "Karcag";
 var_dump($name);
 
-echo"<br>Név: {$name} Életkor: {$age} Lakóhely: {$city}"
+echo"\n<br>Név: {$name} Életkor: {$age} Lakóhely: {$city}";
+//konstans
 
+define("PI", 3.14);
+echo "<br>", PI;
+
+//adattipusok: string, int, float, bool
+
+    $message = "1";
+    echo "Kiir", $message, "értéket.","<br>\n";
+    echo "Kiir $message értéket.<br>\n";
+    echo "Kiir {$message}darab értéket.<br>\n";
+    echo 'Kiir {$message}darab értéket.<br>\n';
+
+    print "Kiir". $message. "értéket."."<br>\n";
+    print "Kiir {$message}darab értéket.<br>\n";
+    print 'Kiir {$message}darab értéket.<br>\n';
+
+    /*
+    git parancsok
+        git init: helyi repo inicializálás
+        git add . :modosított fájlok hozzáadása a staging area-hoz.
+        git commit -m "message": változtatott fájlok commitálása
+        git remote add origin https://github.com/felhasznaloneve.git: hozzárendeljük a helyi repot a távoli repohoz
+        git config --global user.name ill email:
+        git branch -M main : branch főágnév elnevezése
+        git push .u origin main: a helyi repo változásait feltölti a távol repoban
+
+    
+        hf: töltsd le otthon a repot a saját htdocs mappádba
+    */
 ?>
