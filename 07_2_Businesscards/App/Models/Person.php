0f7d5d6198aa601e377499232e2af0c8e093dcd4
Adrian061022 <markusadrian2006@gmail.com>
2025-10-02 11:00:37 +0200

    models done

---
+++ 07_2_Businesscards/App/Models/Person.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+class Person
+{
+    protected ?int $id;
+    public string $name;
+    protected string $email;
+    private string $phone;
+
+
+    public function __construct(?int $id, string $name, string $email, string $phone)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->phone = $phone;
+    }
+    //accessor
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    //getter
+    public function setPhone($phone): string{
+        $this->phone=$phone
+    }
+
+    
+}
+?>
